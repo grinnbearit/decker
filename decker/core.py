9756b6f5cd4a3be7f8225985ec7aee937fa21ddf
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2020-04-05 22:54:11 +0200

    Framework for updating existing wallpapers

--- decker/core.py
+++ decker/core.py
@@ -4,7 +4,7 @@ import operator as o
 from PIL import Image
 import itertools as it
 from collections import defaultdict
-from swissknife.collections import OrderedSet
+from swissknife.collections import OrderedSet, OrderedDefaultDict
 
 
 # Some editions are reserved keywords in windows
@@ -106,30 +106,38 @@ def read_codex(codex_file):
     return acc
 
 
-def _read_editions(codex, newest, oldest, ignore):
+def filter_editions(codex, newest, oldest, ignore, current):
     """
-    helper function for `read_cardex`
-    returns a list of relevant editions given constraints
+    returns two lists of editions, (new, old) split at current, sorted
+    from newest to oldest
+
+    only considers editions between `newest` and `oldest`, ignores `ignore`
     """
     newest_flag = newest is None
-    oldest_flag = False
+    current_flag = False
 
-    acc = []
+    newacc = []
+    oldacc = []
     for row in codex:
         edition = row["edition"]
 
         if newest == edition:
             newest_flag = True
 
+        if current == edition:
+            current_flag = True
+
         if newest_flag and\
-           not oldest_flag and\
            row["edition"] not in ignore:
-            acc.append(edition)
+            if current_flag:
+                oldacc.append(edition)
+            else:
+                newacc.append(edition)
 
         if oldest == edition:
-            oldest_flag = True
+            break
 
-    return acc
+    return (newacc, oldacc)
 
 
 def read_cardex(path, codex, newest=None, oldest=None, ignore=set()):
@@ -144,11 +152,42 @@ def read_cardex(path, codex, newest=None, oldest=None, ignore=set()):
     ignore: a set of editions to ignore
     """
     acc = defaultdict(OrderedSet)
-    for edition in _read_editions(codex, newest, oldest, ignore):
+    (editions, _) = filter_editions(codex, newest, oldest, ignore, None)
+    for edition in editions:
         edfile = EDDEX[edition] if edition in EDDEX else edition
         with open(f"{path}/{edfile}.json", "r") as fp:
             reader = csv.DictReader(fp)
             for line in fp.readlines():
                 row = json.loads(line)
                 acc[row["name"]].add(edition)
+    return acc
+
+
+def read_artex(path, codex, newest=None, oldest=None, ignore=set(), current=None):
+    """
+    Using a codex, returns a map of {artist_id: {illustration_id: {new: [pngid], old: [pngid]} where
+    pngids are sorted from oldest to newest. `new` are pngids released after `current` and `old`
+    includes `current`.
+
+    `illustration_ids` are stored in an OrderedDict, sorted from oldest to newest
+
+    path: the editions path
+    codex: a sorted list of downloaded editions
+    newest: the newest edition to consider
+    oldest: the oldest edition to consider
+    ignore: a set of editions to ignore
+    current: the current edition
+    """
+    acc = defaultdict(lambda: OrderedDefaultDict(lambda: defaultdict(list)))
+    (neweds, oldeds) = filter_editions(codex, newest, oldest, ignore, current)
+    edlist = [("new", e) for e in neweds] + [("old", e) for e in oldeds]
+    edlist.reverse()
+    for (old_new, edition) in edlist:
+        edfile = EDDEX[edition] if edition in EDDEX else edition
+        with open(f"{path}/{edfile}.json", "r") as fp:
+            reader = csv.DictReader(fp)
+            for line in fp.readlines():
+                row = json.loads(line)
+                for artist_id in row["artist_ids"]:
+                    acc[artist_id][row["illustration_id"]][old_new].append(row["pngid"])
     return acc
---
+++ genwall.py
@@ -0,0 +1,109 @@
+import argparse
+import decker.core as dc
+import decker.layout as dl
+from collections import defaultdict
+
+
+def artist_reprints(artex):
+    """
+    returns {artist: [(old_pngid, new_pngid)]} for all their illustrations
+    from oldest to newest
+    """
+    acc = defaultdict(list)
+    for (artist, illustrations) in artex.items():
+        for (illustration, old_new) in illustrations.items():
+            acc[artist].append((old_new["old"][-1] if "old" in old_new else None,
+                                old_new["new"][-1] if "new" in old_new else None))
+    return acc
+
+
+def generate_pnglists(artprints, length):
+    """
+    returns {"new": [pnglist], "unchanged": [pnglist], "updated": [(old_pnglist, new_pnglist)]}
+    where `length` determines the number of pngs in the pnglist
+    """
+    acc = {"new": [], "unchanged": [], "updated": []}
+    for (artist, reprints) in artprints.items():
+        for chunk in [reprints[x:x+length] for x in range(0, len(reprints), length)]:
+            (chunked_old_pngids, chunked_new_pngids) = zip(*chunk)
+            old_pngids = [pngid for pngid in chunked_old_pngids if pngid]
+            new_pngids = [pngid for pngid in chunked_new_pngids if pngid]
+            if old_pngids and new_pngids:
+                acc["updated"].append((old_pngids, new_pngids))
+            elif old_pngids:
+                acc["unchanged"].append(old_pngids)
+            else:
+                acc["new"].append(new_pngids)
+    return acc
+
+
+def encode_pngid(pngid):
+    """
+    converts a pngid into a string
+    """
+    (edition, page, column, row) = pngid
+    offset = page * 100 + column * 10 + row
+    return f"{edition}{offset}"
+
+
+def encode_pnglist(pnglist):
+    """
+    encodes a list of pngids into a string
+    """
+    return "".join(encode_pngid(pngid) for pngid in pnglist)
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-p", "--path",
+                        help="editions directory",
+                        default="editions")
+    parser.add_argument("-i", "--ignore",
+                        help="ignored editions",
+                        nargs='+')
+    parser.add_argument("-n", "--newest",
+                        help="newest edition to consider")
+    parser.add_argument("-o", "--oldest",
+                        help="oldest edition to consider")
+    parser.add_argument("-c", "--current",
+                        help="current edition (last updated)")
+    parser.add_argument("-r", "--remove",
+                        help="prints a command to delete updated pngs",
+                        action="store_true")
+    parser.add_argument("-l", "--layout",
+                        help="wallpaper layout",
+                        nargs=2, type=int, default=(1, 3))
+
+    args = parser.parse_args()
+
+    edset = set(args.ignore) if args.ignore else set()
+    codex = dc.read_codex("codex.csv")
+    artex = dc.read_artex(args.path, codex, args.newest, args.oldest, edset, args.current)
+    artprints = artist_reprints(artex)
+
+    (rows, columns) = args.layout
+    categorised = generate_pnglists(artprints, rows*columns)
+
+    if args.remove:
+        files = [encode_pnglist(old_pnglist) + ".png"
+                 for (old_pnglist, _) in categorised["updated"]]
+        print("rm " + " ".join(files))
+        exit(0)
+
+    if categorised["new"]:
+        print("new:")
+        for new_pnglist in categorised["new"]:
+            print(encode_pnglist(new_pnglist))
+        print()
+
+    if categorised["updated"]:
+        print("updated:")
+        for (old_pnglist, new_pnglist) in categorised["updated"]:
+            print("{} -> {}".format(encode_pnglist(old_pnglist),
+                                    encode_pnglist(new_pnglist)))
+        print()
+
+    if categorised["unchanged"]:
+        print("unchanged:")
+        for old_pnglist in categorised["unchanged"]:
+            print(encode_pnglist(old_pnglist))
