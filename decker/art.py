3f5d4af05935743d68095adc36ea915c566dd14c
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2020-05-08 16:27:30 +0200

    Pull out genwall functions into the da module

--- decker/art.py
+++ decker/art.py
@@ -1,8 +1,59 @@
 import operator as o
+import itertools as it
 import decker.codex as dx
+import decker.layout as dl
 import decker.edition as de
 from collections import OrderedDict
 from swissknife.collections import OrderedDefaultDict
+
+
+def generate_pnglists(wallex, length=3, minimum=1, rollover=True):
+    """
+    Wallex should be an Ordered Dictionary of {category: [pngids]}
+
+    Returns a list of pnglists where each pnglist contains pngids from the same category
+
+    length: the number of pngs in each pnglist
+    minimum: the number of items necessary for a category to be consisdered
+    rollover: If True, ensures that the last pnglist generated adds items from the one before to reach `length`
+    """
+    acc = []
+    for (_, pngids) in wallex.items():
+        if len(pngids) > minimum:
+            for chunk in [pngids[x:x+length] for x in range(0, len(pngids) - (len(pngids) % length), length)]:
+                acc.append(tuple(chunk))
+
+            if rollover:
+                acc.append(tuple(pngids[-length:]))
+            else:
+                acc.append(tuple(pngids[-(len(pngids) % length):]))
+    return acc
+
+
+def render_pnglists(path, pnglists):
+    """
+    returns imglists corresponding to the passed pnglists
+    """
+    images = de.render_pngids(path, list(it.chain(*pnglists)))
+    cropped = [dl.border_crop(image) for image in images]
+    indices = list(it.accumulate([len(pnglist) for pnglist in pnglists]))
+    return [cropped[start:stop] for (start, stop) in zip([0]+indices, indices)]
+
+
+def encode_pngid(pngid):
+    """
+    converts a pngid into a string
+    """
+    (edition, page, column, row) = pngid
+    offset = page * 100 + column * 10 + row
+    return f"{edition}{offset}"
+
+
+def encode_pnglist(pnglist):
+    """
+    encodes a list of pngids into a string
+    """
+    return "".join(encode_pngid(pngid) for pngid in pnglist)
 
 
 def read_artex(path, codex, newest=None, oldest=None, ignore=set()):
--- genwall.py
+++ genwall.py
@@ -1,49 +1,8 @@
 import os
 import argparse
-import itertools as it
 import decker.art as da
 import decker.codex as dx
 import decker.layout as dl
-import decker.edition as de
-from collections import defaultdict
-
-
-def generate_pnglists(artex, length):
-    """
-    returns a list of pnglists where each pnglist contains pngids from the same artist
-    `length` determines the number of pngs in the pnglist
-    """
-    acc = []
-    for (_, pngids) in artex.items():
-        for chunk in [pngids[x:x+length] for x in range(0, len(pngids), length)]:
-            acc.append(tuple(chunk))
-    return acc
-
-
-def render_pnglists(path, pnglists):
-    """
-    returns imglists corresponding to the passed pnglists
-    """
-    images = de.render_pngids(path, list(it.chain(*pnglists)))
-    cropped = [dl.border_crop(image) for image in images]
-    indices = list(it.accumulate([len(pnglist) for pnglist in pnglists]))
-    return [cropped[start:stop] for (start, stop) in zip([0]+indices, indices)]
-
-
-def encode_pngid(pngid):
-    """
-    converts a pngid into a string
-    """
-    (edition, page, column, row) = pngid
-    offset = page * 100 + column * 10 + row
-    return f"{edition}{offset}"
-
-
-def encode_pnglist(pnglist):
-    """
-    encodes a list of pngids into a string
-    """
-    return "".join(encode_pngid(pngid) for pngid in pnglist)
 
 
 if __name__ == "__main__":
@@ -81,11 +40,11 @@ if __name__ == "__main__":
     igset = set(args.ignore) if args.ignore else set()
     codex = dx.read_codex("codex.csv")
     artex = da.read_artex(args.path, codex, args.newest, args.oldest, igset)
-    pnglists = generate_pnglists(artex, 3)
+    pnglists = da.generate_pnglists(artex, 3)
 
     if args.current:
         old_artex = da.read_artex(args.path, codex, args.current, args.oldest, igset)
-        old_pnglists = generate_pnglists(old_artex, 3)
+        old_pnglists = da.generate_pnglists(old_artex, 3)
     else:
         old_pnglists = []
 
@@ -93,24 +52,24 @@ if __name__ == "__main__":
         obs_pnglists = [pnglist for pnglist in old_pnglists if pnglist not in set(pnglists)]
         if args.show:
             for pnglist in obs_pnglists:
-                print(encode_pnglist(pnglist))
+                print(da.encode_pnglist(pnglist))
             print(len(obs_pnglists))
         else:
             for pnglist in obs_pnglists:
-                os.remove("{}/{}.png".format(args.wallpapers, encode_pnglist(pnglist)))
+                os.remove("{}/{}.png".format(args.wallpapers, da.encode_pnglist(pnglist)))
         exit(0)
 
     new_pnglists = [pnglist for pnglist in pnglists if pnglist not in set(old_pnglists)]
 
     if args.show:
         for pnglist in new_pnglists:
-            print(encode_pnglist(pnglist))
+            print(da.encode_pnglist(pnglist))
         print(len(new_pnglists))
         exit(0)
 
     sublists = new_pnglists[(args.start or 0):(args.end or -1)]
-    imglists = render_pnglists(args.path, sublists)
+    imglists = da.render_pnglists(args.path, sublists)
     for (pnglist, imglist) in zip(sublists, imglists):
         sheets = dl.layout(imglist, (1, len(imglist)))
-        filename = "{}/{}.png".format(args.wallpapers, encode_pnglist(pnglist))
+        filename = "{}/{}.png".format(args.wallpapers, da.encode_pnglist(pnglist))
         dl.write_sheets(filename, sheets)
