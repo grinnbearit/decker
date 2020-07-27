7a09cb26bcbd6f86faabfc8b10202810bae8bacc
Sidhant Godiwala <sidhant.godiwala@gmail.com>
2020-07-27 19:36:42 +0200

    Handle Planechase card backs

--- decker/tts.py
+++ decker/tts.py
@@ -7,8 +7,12 @@ def card_to_objectstate(index, edition, collector_number):
     """
     card = index[edition][collector_number]
     face_url = card["image_uris"]["png"]
+
     # taken from https://github.com/jeandeaual/tts-deckconverter
-    back_url = "http://cloud-3.steamusercontent.com/ugc/998016607072060763/7AFEF2CE9E7A7DB735C93CF33CC4C378CBF4B20D/"
+    if card["layout"] in ["normal", "token", "split", "flip"]:
+        back_url = "http://cloud-3.steamusercontent.com/ugc/998016607072060763/7AFEF2CE9E7A7DB735C93CF33CC4C378CBF4B20D/"
+    elif card["layout"] == "planar":
+        back_url = "http://cloud-3.steamusercontent.com/ugc/998016607072060000/1713AE8643632456D06F1BBA962C5514DD8CCC76/"
 
     object_state = {
         "FaceURL": face_url,
@@ -26,7 +30,7 @@ def card_to_description(card):
     """
     returns a description given a card layout
     """
-    if card["layout"] in ["normal", "token"]:
+    if card["layout"] in ["normal", "token", "planar"]:
         description = "[b]{}[/b]\n\n{}".format(card["type_line"], card["oracle_text"])
     elif card["layout"] in ["split", "flip"]:
         acc = []
