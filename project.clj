aa701dabea2b05472be8655e1150dfdcd058ea8f
Sidhant Godiwala <sgodiwala@fb.com>
2021-11-12 12:01:27 +0000

    Scrape Scryfall all sets and save it in edn

--- project.clj
+++ project.clj
@@ -2,5 +2,6 @@
   :description "Generates MtG decks for TTS and Proxying"
   :license {:name "EPL-2.0 OR GPL-2.0-or-later WITH Classpath-exception-2.0"
             :url "https://www.eclipse.org/legal/epl-2.0/"}
-  :dependencies [[org.clojure/clojure "1.10.3"]]
+  :dependencies [[org.clojure/clojure "1.10.3"]
+                 [enlive "1.1.6"]]
   :repl-options {:init-ns decker.core})
--- src/decker/core.clj
+++ src/decker/core.clj
@@ -1,1 +1,37 @@
-(ns decker.core)
+(ns decker.core
+  (:require [net.cgrand.enlive-html :as html]
+            [clojure.string :as str]
+            [clojure.instant :as inst]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]))
+
+
+(defn fetch-all-sets
+  "returns a list of sets found at https://scryfall.com/sets"
+  []
+  (let [content (html/html-resource (java.net.URL. "https://scryfall.com/sets"))]
+    (for [row (html/select content [:table.checklist :> :tbody :tr])
+          :when (= (first (html/select row [:a.pillbox-item html/text])) "en")
+          :let [[set-name _ set-code _ cardinality date] (->> (drop 1 (html/select row [:td :a html/text]))
+                                                              (drop-while map?))]]
+      #:all-sets{:set-name (str/trim set-name)
+                 :set-code set-code
+                 :cardinality (Integer/parseInt cardinality)
+                 :date (inst/read-instant-date date)})))
+
+
+(defn write-all-sets!
+  "Writes a list of all-sets to all_sets.edn"
+  [all-sets]
+  (with-open [f (io/writer "all_sets.edn")]
+    (binding [*out* f]
+      (pr all-sets))))
+
+
+(defn read-all-sets
+  "Reads the current list of all-sets from all_sets.edn
+
+  If the file doesn't exist, returns ()"
+  []
+  (with-open [f (io/reader "all_sets.edn")]
+    (edn/read (java.io.PushbackReader. f))))
