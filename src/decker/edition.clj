0aa4825a0450a742341bebd62974c215fa0c8da9
Sidhant Godiwala <sgodiwala@fb.com>
2022-04-12 12:55:40 +0530

    Clean Card Metadata

---
+++ src/decker/edition.clj
@@ -0,0 +1,78 @@
+(ns decker.edition
+  (:require [clj-http.client :as client]
+            [clojure.pprint :as pprint]
+            [net.cgrand.enlive-html :as html]
+            [clojure.string :as str]
+            [clojure.instant :as inst]
+            [clojure.java.io :as io]
+            [clojure.edn :as edn]))
+
+
+(defn !fetch-edition-list
+  "returns a list of editions found at https://scryfall.com/sets"
+  []
+  (let [content (html/html-resource (java.net.URL. "https://scryfall.com/sets"))]
+    (for [row (html/select content [:table.checklist :> :tbody :tr])
+          :when (= (first (html/select row [:a.pillbox-item html/text])) "en")
+          :let [[edition-name _ edition-code _ cardinality date] (->> (drop 1 (html/select row [:td :a html/text]))
+                                                                      (drop-while map?))]]
+      #:edition{:name (str/trim edition-name)
+                :code edition-code
+                :cardinality (Integer/parseInt cardinality)
+                :date (inst/read-instant-date date)})))
+
+
+(defn write-edition-list!
+  "Writes a list of editions to metadata/editions.edn"
+  [edition-list]
+  (with-open [f (io/writer "metadata/editions.edn")]
+    (binding [*out* f]
+      (pprint/pprint edition-list))))
+
+
+(defn !read-edition-list
+  "Reads the current list of editions from metadata/editions.edn
+
+  If the file doesn't exist, returns ()"
+  []
+  (with-open [f (io/reader "metadata/editions.edn")]
+    (edn/read (java.io.PushbackReader. f))))
+
+
+(defn !fetch-edition
+  "Given the 3 letter code for a edition, returns a list of card metadata from scryfall"
+  [edition]
+  (letfn [(->card [scryfall-card]
+            #:card{:collector-number (:collector_number scryfall-card)
+                   :name (:name scryfall-card)})]
+
+    (loop [acc [] page 1 has-more? true]
+      (if (not has-more?)
+        (->> (apply concat acc)
+             (map ->card))
+        (let [response (client/get "https://api.scryfall.com/cards/search"
+                                   {:query-params {"order" "set"
+                                                   "q" (format "e:%s unique:prints" edition)
+                                                   "page" page}
+                                    :as :json})]
+          (Thread/sleep 50)
+          (recur (conj acc (get-in response [:body :data]))
+                 (inc page)
+                 (get-in response [:body :has_more])))))))
+
+
+(defn write-edition!
+  "writes edition metadata to a file named metadata/{edition}_metadata.edn"
+  [edition card-list]
+  (let [filename (format "metadata/%s_metadata.edn" edition)]
+    (with-open [f (io/writer filename)]
+      (binding [*out* f]
+        (pprint/pprint card-list)))))
+
+
+(defn !read-edition
+  "returns a list of card metadata for the passed edition"
+  [edition]
+  (let [filename (format "metadata/%s_metadata.edn" edition)]
+    (with-open [f (io/reader filename)]
+      (edn/read (java.io.PushbackReader. f)))))
